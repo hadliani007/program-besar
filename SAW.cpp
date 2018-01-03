68caefdbffa5c45ead6f316c8dc6bb706264ceca
hadliani007 <hadliani007@gmail.com>
2018-01-03 20:29:04 +0800

    yang petama

--- SAW.cpp
+++ SAW.cpp
@@ -2,4 +2,11 @@
 #include <cmath>
 using namespace std;
 
-int main()
+int main(){
+	cout.setf(ios::fixed); cout.setf(ios::showpoint); cout.precision(4);
+	//x
+	double a1c[5]={4,4,5,3,3}, a2c[5]={3,3,4,2,3}, a3c[5]={5,4,2,2,2};
+	//x
+	cout << "MATRIX X :"<<endl;
+	int kuda1=0;
+	do{
